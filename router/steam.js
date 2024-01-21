45dcdcc8a2dff0e949874b0af5a15be62810967d
luisp-rimac <xt7970@rimac.com.pe>
2024-01-20 20:39:37 -0500

    Cambiar el apikey

--- router/steam.js
+++ router/steam.js
@@ -3,7 +3,7 @@ import { Router, json } from "express";
 const steamRouter = Router();
 
 // CONSTANTES
-const apiKey = "D6898510C87B2E34A32FCBDDDCDF2FED";
+const apiKey = "259B254C049DC2858AD4C5D6E913658F";
 const steamFriendId = "76561199619562954"; 
 const appipdota2 = "570";
 
@@ -34,7 +34,7 @@ steamRouter.get("/user", async(req, res) => {
 
 // ********************************************************
 steamRouter.post("/create-lobby", async(req, res) => {
-  const createLobby = `https://partner.steam-api.com/ILobbyMatchmakingService/CreateLobby/v1?key=${apiKey}&appid=${appipdota2}&max_members=10&lobby_type=4&lobby_name=amigos&steamid_invited_members=[]`;
+  const createLobby = `https://partner.steam-api.com/ILobbyMatchmakingService/CreateLobby/v1?key=${apiKey}&appid=${appipdota2}&max_members=10&lobby_type=4&lobby_name=amigos&steamid_invited_members=['${steamFriendId}']`;
 
   try {
     const response = await fetch(createLobby, { 
@@ -43,21 +43,21 @@ steamRouter.post("/create-lobby", async(req, res) => {
         'Content-Type': 'application/x-www-form-urlencoded'
       }
     });
-    const data = await response.json();
-
-    if (response.ok) {
+    console.log("response", response);
+    if (response.status === 200) {
       res.json({
         message: "CREATE LOBBY",
         data
       });
     } else {
       // Maneja el error
-      res.status(403).json({
+      res.status(response.status).json({
         message: "ERROR AL CREAR LOBBY 1",
+        informacion: response.statusText
       });
     }
   } catch (error) {
-    res.status(403).json({
+    res.status(40).json({
       message: "ERROR AL CREAR LOBBY 2",
       error: JSON.stringify(error)
     });
